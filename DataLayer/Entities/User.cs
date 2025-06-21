6e65bf1c040200478a3bc16009c88f45f42181cc
TahaAyati <TahaAyati2007@gmail.com>
2025-06-21 17:23:46 +0330

    First commit

---
+++ DataLayer/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public ICollection<PostComment> PostComments { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+    enum UserRole
+    {
+        Admin,
+        User,
+        Writer
+    }
+}
