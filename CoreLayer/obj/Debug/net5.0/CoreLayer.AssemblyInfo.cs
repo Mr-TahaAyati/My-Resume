a5ebd58ef1a15b0ddc7bd4722324513183c3e6b1
TahaAyati <TahaAyati2007@gmail.com>
2025-06-23 21:07:59 +0330

    feat: add initial website template

--- CoreLayer/obj/Debug/net5.0/CoreLayer.AssemblyInfo.cs
+++ CoreLayer/obj/Debug/net5.0/CoreLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e65bf1c040200478a3bc16009c88f45f42181cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc0f59c0dca7382255418b6d32b5fe05012511b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
